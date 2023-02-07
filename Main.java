825f1b3a06c71bdec273974f84d60bcf421d4698
Drew <drewpyatt97@gmail.com>
2023-02-06 20:59:47 -0500

    add Main.java

---
+++ Main.java
@@ -0,0 +1,13 @@
+/*
+Name: Andrew Pyatt
+Class: CSET 3600
+Instructor: Don Miller
+Homework: 1
+Date: 020623
+ */
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
